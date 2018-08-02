2a513d69fc5b7641c8d56f8a9db78ae4715b0598
unknown <leabney@gmail.com>
2018-08-02 12:48:49 -0500

    Updates to bio

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+function displayLinks() {
+    var link = document.getElementById("moreLink");
+    if (link.style.display === "none") {
+        link.style.display = "inline";
+    } else {
+        link.style.display = "none";
+    }
+}
